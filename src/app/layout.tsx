c0d22c3aaee4d907ff6610ce7c5910d67fdf27ad
Parth Arora <parth@example.com>
2026-05-22 02:35:10 +0000

    Add neon banana scroll to top button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export const metadata: Metadata = {
 
 import Sidebar from "@/components/Sidebar";
 import AntiInspect from "@/components/AntiInspect";
+import ScrollToTop from "@/components/ScrollToTop";
 
 export default function RootLayout({
   children,
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className="min-h-full flex bg-black text-white overflow-x-hidden">
         <Sidebar />
         <AntiInspect />
+        <ScrollToTop />
         <div className="flex-1 flex flex-col min-h-screen relative md:ml-[260px] lg:ml-0 w-full md:w-[calc(100%-260px)] lg:w-full">
           <Navbar />
           <IPTracker />
